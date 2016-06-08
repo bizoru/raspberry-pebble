f2146e5bc9bad525355a608f210ca3f7cdc3fffb
Steven Sierra Forero <bizoru@gmail.com>
2016-06-07 19:06:16 -0500

    Fix to raspberry client

--- raspberry_client.py
+++ raspberry_client.py
@@ -1,7 +1,33 @@
 import requests
+import RPi.GPIO as GPIO
+import time
+import sys
 
-base_url = "http://localhost:3412/status"
-r = requests.get(base_url)
+url ="localhost:3412"
 
-result = r.json()
-print result['status']
+if len(sys.argv) > 1:
+    base_url = sys.argv[1]
+
+# Setup things
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(18,GPIO_OUT)
+
+
+
+def get_status():
+    url = "http://{}/status".format(base_url)
+    r = requests.get(url)
+    result = r.json()
+    return result['status']
+
+def monitor_status():
+    while True:
+        time.sleep(0.3)
+        status = get_status()
+        print "Got status {}".format(status)
+        if status == "on":
+            GPIO.output(18,GPIO.HIGH)
+        if status == "off":
+            GPIO.output(18,GPIO.LOW)
+
+monitor_status()
